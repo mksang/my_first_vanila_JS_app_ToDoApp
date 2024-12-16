3ac25ccdafc2b2b7567ff65e42cab688f184d9cb
mksang <mukesh.sangwan001@gmail.com>
2024-12-16 10:46:44 +0530

    made few updates in css

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const root = document.querySelector('#root')
 
 const appName = document.createElement('h1')
-appName.innerText = 'Focus On Today'
+appName.innerText = 'Focus On '
 appName.classList.add('heading-container')
 root.append(appName)
+
+const headingSpan = document.createElement('span')
+headingSpan.innerText = 'Today'
+headingSpan.classList.add('span1')
+appName.append(headingSpan)
 
 const borderDiv = document.createElement('div')
 borderDiv.classList.add('main-container')
@@ -14,64 +19,110 @@ p_today.classList.add('today-container')
 p_today.innerText = 'Today'
 borderDiv.append(p_today)
 
+const sunImg = document.createElement('img')
+sunImg.src = 'images/Sun.png'
+sunImg.classList.add('sunimg')
+p_today.append(sunImg)
+
+
 
 const pForBar = document.createElement('p')
 pForBar.innerText= 'Raise the bar by completing your goals'
 pForBar.classList.add('bar-container')
 borderDiv.append(pForBar)
 
-const raisingInput = document.createElement('input')
-raisingInput.classList.add('raisingInput-container')
-raisingInput.setAttribute('readonly', true);
-
-borderDiv.append(raisingInput)
+const progessbar = document.createElement('div')
+progessbar.classList.add('progessbar-container')
+borderDiv.append(progessbar)
+const progessvalue = document.createElement('div')
+progessvalue.classList.add('progessbar-value')
+progessbar.append(progessvalue)
 
 const warningP = document.createElement('p')
 warningP.classList.add('setgoal-div')
-warningP.innerText = 'set the goal'
+warningP.innerText = 'Please set all the 3 goals!'
 borderDiv.append(warningP)
 
-const list1 = document.createElement('div')
-list1.setAttribute('contenteditable', 'true');
+//input 1
 
-list1.classList.add('list1')
-borderDiv.append(list1)
+const divforgoal1 = document.createElement('div')
+divforgoal1.classList.add('divforgoal1-container')
+borderDiv.append(divforgoal1)
 
-const radioButton = document.createElement('input');
-radioButton.type = 'radio';
-radioButton.classList.add('radio-button1'); 
-list1.appendChild(radioButton);
+const radioInput1 = document.createElement('input')
+radioInput1.classList.add('radio1')
+radioInput1.type = 'radio'
+divforgoal1.append(radioInput1)
 
-// list2
-const list2 = document.createElement('div')
-list2.setAttribute('contenteditable', 'true');
+const input1 = document.createElement('input')
+input1.classList.add('input1-container')
+input1.type = 'text'
+input1.placeholder = 'Enter your goal';
+divforgoal1.append(input1)
 
-list2.classList.add('list2')
-borderDiv.append(list2)
+const tick1 = document.createElement('img')
+tick1.src = 'images/Vector 1.png'
+tick1.classList.add('tick1')
+radioInput1.append(tick1)
 
-const radioButton2 = document.createElement('input');
-radioButton2.type = 'radio';
-radioButton2.classList.add('radio-button2'); 
-list2.appendChild(radioButton2);
 
-//list3
-const list3 = document.createElement('div')
-list3.setAttribute('contenteditable', 'true');
 
-list3.classList.add('list3')
-borderDiv.append(list3)
 
-const radioButton3 = document.createElement('input');
-radioButton3.type = 'radio';
-radioButton3.classList.add('radio-button3'); 
-list3.appendChild(radioButton3);
+//input 2
+
+const divforgoal2 = document.createElement('div')
+divforgoal2.classList.add('divforgoal2-container')
+borderDiv.append(divforgoal2)
+
+const radioInput2 = document.createElement('input')
+radioInput2.classList.add('radio2')
+radioInput2.type = 'radio'
+divforgoal2.append(radioInput2)
+
+const input2 = document.createElement('input')
+input2.classList.add('input2-container')
+input2.type = 'text'
+input2.placeholder = 'Enter your goal';
+divforgoal2.append(input2)
+
+const tick2 = document.createElement('img')
+tick2.src = 'images/Vector 1.png'
+tick2.classList.add('tick2')
+radioInput2.append(tick2)
+
+
+
+//input 3
+
+const divforgoal3 = document.createElement('div')
+divforgoal3.classList.add('divforgoal3-container')
+borderDiv.append(divforgoal3)
+
+const radioInput3 = document.createElement('input')
+radioInput3.classList.add('radio3')
+radioInput3.type = 'radio'
+divforgoal3.append(radioInput3)
+
+const input3 = document.createElement('input')
+input3.classList.add('input3-container')
+input3.type = 'text'
+input3.placeholder = 'Enter your goal';
+divforgoal3.append(input3)
+
+
+const tick3 = document.createElement('img')
+tick3.src = 'images/Vector 1.png'
+tick3.classList.add('tick3')
+radioInput3.append(tick3)
+
+
 
 const progress = document.createElement('p')
 progress.innerText = '“Keep Going, You’re making great progress!”'
 progress.classList.add('progress-container')
 borderDiv.append(progress)
 const last = document.createElement('p')
-last.innerText = '“Made by Mukesh Kumar”'
+last.innerText = '“Made with ❤️ by Mukesh Kumar”'
 last.classList.add('last-container')
 borderDiv.append(last)
 
