19f496d28585c416003368b17389d0e713794e94
mksang <mukesh.sangwan001@gmail.com>
2024-12-18 11:59:23 +0530

    'today's commit'

--- script.js
+++ script.js
@@ -27,15 +27,17 @@ p_today.append(sunImg)
 
 
 const pForBar = document.createElement('p')
-pForBar.innerText= 'Raise the bar by completing your goals'
+pForBar.innerText = 'Raise the bar by completing your goals'
 pForBar.classList.add('bar-container')
 borderDiv.append(pForBar)
 
 const progessbar = document.createElement('div')
 progessbar.classList.add('progessbar-container')
 borderDiv.append(progessbar)
+
 const progessvalue = document.createElement('div')
 progessvalue.classList.add('progessbar-value')
+
 progessbar.append(progessvalue)
 
 const warningP = document.createElement('p')
@@ -60,10 +62,7 @@ input1.type = 'text'
 input1.placeholder = 'Enter your goal';
 divforgoal1.append(input1)
 
-const tick1 = document.createElement('img')
-tick1.src = 'images/Vector 1.png'
-tick1.classList.add('tick1')
-radioInput1.append(tick1)
+
 
 
 
@@ -81,14 +80,12 @@ divforgoal2.append(radioInput2)
 
 const input2 = document.createElement('input')
 input2.classList.add('input2-container')
+
 input2.type = 'text'
 input2.placeholder = 'Enter your goal';
 divforgoal2.append(input2)
 
-const tick2 = document.createElement('img')
-tick2.src = 'images/Vector 1.png'
-tick2.classList.add('tick2')
-radioInput2.append(tick2)
+
 
 
 
@@ -110,10 +107,7 @@ input3.placeholder = 'Enter your goal';
 divforgoal3.append(input3)
 
 
-const tick3 = document.createElement('img')
-tick3.src = 'images/Vector 1.png'
-tick3.classList.add('tick3')
-radioInput3.append(tick3)
+
 
 
 
@@ -126,4 +120,127 @@ last.innerText = '“Made with ❤️ by Mukesh Kumar”'
 last.classList.add('last-container')
 borderDiv.append(last)
 
+const radioInputs = [radioInput1, radioInput2, radioInput3]
+const progressvalue = document.querySelector('.progessbar-value')
+
+
+
+const radioButton1 = document.getElementsByClassName('radio1')[0]
+const radioButton2 = document.getElementsByClassName('radio2')[0]
+const radioButton3 = document.getElementsByClassName('radio3')[0]
+
+const inputField1 = document.getElementsByClassName('input1-container')[0]
+const inputField2 = document.getElementsByClassName('input2-container')[0]
+const inputField3 = document.getElementsByClassName('input3-container')[0]
+
+// const pForBar = document.querySelector('.bar-container');
+// const progessvalue = document.querySelector('.progessbar-value');
+
+radioButton1.addEventListener('change', () => {
+    if (radioButton1.checked) {
+        inputField1.classList.add('crossed1')
+    }
+    else {
+        inputField1.classList.remove('crossed1')
+
+    }
+})
+
+radioButton2.addEventListener('change', () => {
+    if (radioButton2.checked) {
+        inputField2.classList.add('crossed2')
+    }
+    else {
+        inputField2.classList.remove('crossed2')
+    }
+})
+
+radioButton3.addEventListener('change', () => {
+    if (radioButton3.checked) {
+        inputField3.classList.add('crossed3')
+    }
+    else {
+        inputField3.classList.remove('crossed3')
+    }
+})
+
+
+const tasks = [
+    inputField1,
+    inputField2,
+    inputField3
+];
+
+
+radioButton1.addEventListener('change', () => {
+    if (radioButton1.checked) {
+        pForBar.innerText = `${inputField1.value || ''}`
+        progessvalue.innerText = '1/3 task completed'
+
+    }
+    else {
+        pForBar.innerText = 'Raise the bar by completing your goals'
+        progessvalue.innerText = ''
+
+
+    }
+
+})
+radioButton2.addEventListener('change', () => {
+    if (radioButton2.checked) {
+        pForBar.innerText = `${inputField2.value || ''}`
+        progessvalue.innerText = '2/3 task completed'
+
+    }
+    else {
+        pForBar.innerText = 'Raise the bar by completing your goals'
+        progessvalue.innerText = ''
+
+
+    }
+})
+radioButton3.addEventListener('change', () => {
+    if (radioButton3.checked) {
+        pForBar.innerText = `${inputField3.value || ''}`
+        progessvalue.innerText = 'All task completed'
+
+    }
+    else {
+        pForBar.innerText = 'Raise the bar by completing your goals'
+        progessvalue.innerText = ''
+
+
+    }
+})
+
+const radioButtons = [radioButton1, radioButton2, radioButton3]
+
+radioButtons.forEach((button) => {
+    button.addEventListener('change', () => {
+        if (radioButton1.checked || radioButton2.checked || radioButton3.checked) {
+            progessbar.classList.add('progessbar-value1')
+            pForBar.innerText = `${inputField1.value || ''}`
+            progessvalue.innerText = '1/3 task completed'
+        }
+        else {
+             progessvalue.classList.remove('progessbar-container')
+        }
+        if (radioButton1.checked && radioButton2.checked || radioButton2.checked && radioButton3.checked || radioButton1.checked && radioButton3.checked) {
+            progessbar.classList.add('progessbar-value2')
+            pForBar.innerText = `${inputField2.value || ''}`
+            progessvalue.innerText = '2/3 task completed'
+        }
+        else {
+            // progessvalue.classList.add('progessbar-value')
+        }
+        if (radioButton1.checked && radioButton2.checked && radioButton3.checked ) {
+            progessbar.classList.add('progessbar-value3')
+            pForBar.innerText = `${inputField3.value || ''}`
+            progessvalue.innerText = 'All task completed'
+        }
+        else {
+            // progessvalue.classList.add('progessbar-value')
+        }
+    })
+})
 
